6774d2c38ddc20eaf23fb264c8f813198362793b
wtritch <wtritchler@fairwaytech.com>
2017-04-12 22:05:17 -0700

    Problem 1 initial commit

---
+++ lang-ts/app/src/index.ts
@@ -0,0 +1,9 @@
+
+import Problem1 from "./problem-1";
+
+var problem = new Problem1();
+
+Problem1.bruteForce();
+Problem1.orderedCompositeGenerator();
+Problem1.moddingGenerator();
+Problem1.mathiness();
